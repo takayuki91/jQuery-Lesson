9ebf1a6ac8ae075de52061b5011dfbfd980a745b
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-05-03 12:27:27 +0000

    [Add]jQuery_HTML_CSS

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// 文字列の表示
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// 青色長方形に変更
+// $(function(){
+//   $('.box1').css({
+//     'background-color':'#0000FF',
+//     'height':'100px'
+//   });
+// });
+
+// 上から下にスライド
+// $(function(){
+//   $('.box1').slideDown();
+// });
+
+// 下から上にスライド
+// $(function(){
+//   $('.box1').slideUp();
+
+// 非表示を表示
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color':'#0000FF'});
+// });
+
+// 表示を非表示
+$(function(){
+  $('.box1').hide();
 });
