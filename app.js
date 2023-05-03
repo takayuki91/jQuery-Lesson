a2e12928c4fe7e90c1dc8709ab84be06bf4a4d86
EC2 Default User <takayuki.nakatsuji@gmail.com>
2023-05-03 23:58:52 +0000

    [Add]6-event detection

--- app.js
+++ app.js
@@ -30,3 +30,33 @@
 // $(function(){
 //   $('.box1').hide();
 // });
+
+// マウスオーバーイベント
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color':'#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color':'#FF0000'});
+//   });
+// });
+
+// classを指定
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+//  マウスクリックイベント
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
