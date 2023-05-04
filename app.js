8655b6153fd4eb1168189d18ff32bdc123732846
EC2 Default User <takayuki.nakatsuji@gmail.com>
2023-05-04 00:02:26 +0000

    [Update]6-event detection

--- app.js
+++ app.js
@@ -52,11 +52,11 @@
 // });
 
 //  マウスクリックイベント
-$(function(){
-  $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
-  });
-});
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
